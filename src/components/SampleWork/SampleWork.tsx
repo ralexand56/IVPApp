3b22c53e8f2433cb2f4243e9737d18bb8915ddf1
ralexand56 <ralexand56@live.com>
2018-01-22 19:14:26 -0800

    refactor sample work complete

--- src/components/SampleWork/SampleWork.tsx
+++ src/components/SampleWork/SampleWork.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { StatelessComponent } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import actionCreators from '../../actions/ClientActions';
 import ThemeInterface from '../../theme';
@@ -10,34 +10,10 @@ import { Button, Icon, Input, message, Popconfirm, Upload } from 'antd';
 const Search = Input.Search;
 
 const UploadButton = () => (
-  <form action="upload.php" method="post" encType="multipart/form-data">
-    <Button type="primary" ghost={true}>
-      <Icon type="upload" />Upload Image
-    </Button>
-    {/* <input type="file" name="fileToUpload" id="fileToUpload" />
-      <input type="submit" value="Upload Image" name="submit" /> */}
-  </form>
+  <Button type="primary" ghost={true}>
+    <Icon type="upload" />Upload Image
+  </Button>
 );
-
-interface AppState {
-  imgLinks: SampleLink[];
-}
-
-// const GetGoogleStorageUrl = (path: string): string => {
-//   storage
-//     .ref(`${path}`)
-//     .getDownloadURL()
-//     .then(url => {
-//       console.dir(url);
-//       return url;
-//     })
-//     .catch(y => {
-//       console.dir(y);
-//       return undefined;
-//     });
-
-//   return '';
-// };
 
 const beforeUpload = async (
   file: { type: string; size: number; name: string },
@@ -61,6 +37,14 @@ const beforeUpload = async (
   return isLt2M;
 };
 
+const renderImageLinks = (sampleLinks: SampleLink[]) => {
+  return sampleLinks.map(x => (
+    <div key={x.id}>
+      <img src={x.src} />
+    </div>
+  ));
+};
+
 interface Props {
   className?: string;
   children?: React.ReactChild;
@@ -73,121 +57,53 @@ interface Props {
   updateClient: typeof actionCreators.updateClient;
 }
 
-class SampleWork extends Component<Props, {}> {
-  state: AppState = { imgLinks: [] };
-
-  constructor(props: Props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    this.props.currentClient.sampleLinks &&
-      this.renderImageLinks(this.props.currentClient.sampleLinks);
-  }
-
-  getImageLink = async (s: SampleLink) =>
-    s.isLocal ? await storage.ref(`images/${s.src}`).getDownloadURL() : s.src;
-
-  renderImageLinks = (sampleLinks: SampleLink[]) => {
-    // let elements: string[] = [];
-    // console.dir(sampleLinks);
-    // const promises = sampleLinks.filter(y => y.isLocal).map(async x => (
-    //   <div key={x.id}>
-    //     <img src={await storage.ref(`images/${x.src}`).getDownloadURL()} />
-    //   </div>
-    // ));
-
-    // console.dir(promises);
-
-    // sampleLinks.map(async x => {
-    //   let link: string = x.isLocal ? await this.getImageLink(x) : x.src;
-
-    //   this.setState((prevState: AppState) => ({
-    //     imgLinks: [
-    //       ...prevState.imgLinks,
-    //       { id: x.id, src: link, isLocal: x.isLocal },
-    //     ],
-    //   }));
-    // });
-
-    return sampleLinks.map(x => (
-      <div key={x.id}>
-        <img src={x.src} />
-      </div>
-    ));
-    // console.dir(await elements);
-    // console.dir(elements);
-    // await this.setState(prevState => ({ imgLinks: elements }));
-
-    // Promise.all(elements).then(x => {
-    //   console.dir(x);
-    //   this.setState(prevState => ({ imgLinks: x }));
-    // });
-
-    // console.dir(elements);
-
-    // return <h1>Done</h1>;
-    // return elements.forEach((x, i) => (
-    //   <div key={i}>
-    //     <img src={x} />
-    //   </div>
-    // ));
-  };
-
-  render() {
-    const {
-      addSampleWork,
-      className,
-      currentClient,
-      deleteSampleLink,
-      isInEditMode,
-    } = this.props;
-    // console.dir(this.state.imgLinks);
-
-    return (
-      <div className={className}>
-        {isInEditMode ? (
-          <div>
-            <div style={{ margin: 10 }}>
-              <Search
-                placeholder="add image link"
-                onSearch={val => addSampleWork(val, false, currentClient)}
-              />{' '}
-              or{' '}
-              <Upload
-                beforeUpload={file =>
-                  beforeUpload(file, addSampleWork, currentClient)
-                }
-                name="avatar"
-                listType="text"
-                className="avatar-uploader"
-                showUploadList={false}
-                action="http://ivppublicart.com/admin/upload.php"
-              >
-                <UploadButton />
-              </Upload>
-            </div>
-            {currentClient.sampleLinks && (
-              <StringList
-                label="Image Sources"
-                linkList={renderLinkElements(
-                  currentClient.sampleLinks,
-                  currentClient,
-                  deleteSampleLink,
-                )}
-              />
+const SampleWork: StatelessComponent<Props> = ({
+  addSampleWork,
+  className,
+  currentClient,
+  deleteSampleLink,
+  isInEditMode,
+}) => (
+  <div className={className}>
+    {isInEditMode ? (
+      <div>
+        <div style={{ margin: 10 }}>
+          <Search
+            placeholder="add image link"
+            onSearch={val => addSampleWork(val, false, currentClient)}
+          />{' '}
+          or{' '}
+          <Upload
+            beforeUpload={file =>
+              beforeUpload(file, addSampleWork, currentClient)
+            }
+            name="avatar"
+            listType="text"
+            className="avatar-uploader"
+            showUploadList={false}
+          >
+            <UploadButton />
+          </Upload>
+        </div>
+        {currentClient.sampleLinks && (
+          <StringList
+            label="Image Sources"
+            linkList={renderLinkElements(
+              currentClient.sampleLinks,
+              currentClient,
+              deleteSampleLink,
             )}
-          </div>
-        ) : (
-          <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
-            {currentClient.sampleLinks &&
-              this.renderImageLinks(currentClient.sampleLinks)}
-          </Carousel>
+          />
         )}
       </div>
-    );
-  }
-}
+    ) : (
+      <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
+        {currentClient.sampleLinks &&
+          renderImageLinks(currentClient.sampleLinks)}
+      </Carousel>
+    )}
+  </div>
+);
 
 const renderLinkElements = (
   sampleLinks: SampleLink[],
@@ -202,6 +118,7 @@ const renderLinkElements = (
         title="Are you sure you want to delete the image source?"
         okText="Yes"
         cancelText="No"
+        onConfirm={() => deleteSampleLink(x.id, currentClient)}
       >
         <Button
           size="small"
@@ -209,7 +126,6 @@ const renderLinkElements = (
           style={{
             margin: 5,
           }}
-          onClick={() => deleteSampleLink(x.id, currentClient)}
           type="primary"
         >
           <Icon type="minus" />
@@ -221,6 +137,7 @@ const renderLinkElements = (
 const StyledSampleWork = styled(SampleWork)`
   display: flex;
   width: 100%;
+  font-size: 0.9em;
   justify-content: ${props => (props.isInEditMode ? 'flex-start' : 'center')};
   img {
     display: flex;
